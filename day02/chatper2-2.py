143f974e41a1ce843f34ef1cded3316d1a3562eb
itdanja2025 <itdanja2025@gmail.com>
2026-04-16 12:35:35 +0900

    day02

---
+++ day02/chatper2-2.py
@@ -0,0 +1,28 @@
+# 숫자
+print( 273 )        # print( ) vs console.log( ) vs System.out.print( )
+print( 53.273 )
+
+# 타입 확인 , type( 자료 ) 
+print( type( 52 ) ) # integer
+print( type( 52.273 ) ) # float , 부동소수점
+
+# 지수표현 : 지수 승 표현 방법 , 자료e지수승
+0.52273e2
+0.52273e-2
+
+# 연산자 
+print( 5 + 7 ) # 12 
+print( 5 - 7 ) # -2
+print( 5 * 7 ) # 35
+print( 5 / 7 ) # 0.714 ~~~ 
+print( 10 // 3 ) # 3 , 몫*  
+print( 10 % 3 ) # 1 , 나머지
+print( 2 ** 3 ) # 8 , 제곱 연산자 
+
+# 함수명A( 함수명B( 함수명C( ) ) ) # C -> B -> A 
+# 함수명A().함수명B().함수명C( ) # A -> B -> C
+
+print( "안녕" + "하세요" * 3)
+
+
+
