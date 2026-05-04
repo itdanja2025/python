0a11934196b80f81ad48ad43f3eee819196bf80e
itdanja2025 <itdanja2025@gmail.com>
2026-05-04 15:17:17 +0900

    day13

--- day13/T3-04.py
+++ day13/T3-04.py
@@ -62,7 +62,46 @@ plt.title( '음식 선호도' )
 plt.legend()
 plt.show()
 
+# [5] 선점도 , 밀집도 , .scatter( x축값 , y축값 , c(color) = '색상' , s = 점크기 )
+x = [ 1.5, 2.5, 3.5, 4.5, 5.5 ]
+y = [ 50 , 60 , 65 , 70 , 75 ]
+plt.scatter( x , y  , c = 'red' , s = 100 )
+plt.grid()
+plt.show()
 
+# [6] 히스토그램 , 상관관계 , .hist( 값 , color='' , alpha = 투명도 , bins=구간개수 )
+# 샘플 데이터 
+data = [] 
+for i in range( 500 ) :  # 500번 반복 
+    value = sum( [ (i * j) % 100 / 100 for j in range( 1 , 13 ) ] )    # 리스트 내포 ( 1차원 리스트 생성 )
+    # (i*j) % 100 : 나머지값 계산
+    # /100 : 0 ~ 1 사이 값으로 계산 
+    # sum( ) : 총합계 , 즉] 13개의 0~1 사이의 난수를 만들어서 총합계 ( 중앙값이 큰 난수 생성될 예정 )
+    data.append( value )
+# 차트 만들기 
+plt.hist( data , color= 'skyblue' , alpha = 0.7 , bins= 30 )
+plt.show() 
+
+# [7] 다중 그래프 표현 , .subplots( 행개수 , 열개수 , figsize = ( 가로 , 세로 ) ) 
+fig , axs = plt.subplots( 1 , 2 , figsize=( 10 , 7 ) )    # 한줄에 2개 차트 
+# fig : 다중 그래프를 가지고 있는 전체 그래프 
+# axs : 다중 그래프의 위치 , axs[0] 첫번째 그래프 , axs[1] 두번째 그래프
+# figsize = ( 가로, 세로 ) , 전체 그래프의 가로inch / 세로inch
+axs[0].plot( [1,2,3] , [1,4,9] )
+axs[0].set_title('선그래프')            # 주의할점 : .title( ) 전체 그래프의 제목 , .set_title( ) 하위 그래프의 제목 
+axs[0].set_xlabel('x축제목')            # 즉] 하위 .title , xlabel , ylabel 등 에서는 set_XXXX( ) 으로 사용하기.
+axs[0].set_ylabel('y축제목')
+axs[1].bar( [1,2,3] , [3,5,2] )
+axs[1].set_title('막대그래프')
+axs[1].set_xlabel('x축제목')
+axs[1].set_ylabel('y축제목')
+plt.savefig('./day13/save_chart.png') # 그래프 이미지 다운로드 , .savefig('파일경로')
+plt.show()
+# fig, axs = plt.subplots( 2 , 2 )  # 행=2 , 열=2 , 총 그래프수 = 4
+# axs[0][0].plot()
+# axs[0][1].bar()
+# axs[1][0].scatter()
+# axs[1][1].pie()
 
 
 
